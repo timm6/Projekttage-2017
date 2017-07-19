1d2f6ea405148d5f29b55f9e85979d94cb582233
Robin Grether <privat@robingrether.de>
2017-07-19 09:25:16 +0200

    Übung

--- Lektion 2/aufgabe-geburtstag.py
+++ Lektion 2/aufgabe-geburtstag.py
@@ -6,15 +6,15 @@ day = int(input("Tag: "))
 month = int(input("Monat: "))
 year = int(input("Jahr: "))
 
-if month < 7 or (month == 7 and day < 18):    # vor November oder vor dem 21. November
+if month < 7 or (month == 7 and day < 19):    # vor Juli oder vor dem 19. Juli
     print("Sie hatten dieses Jahr bereits Geburtstag.")
-elif month == 7 and day == 18:                 # genau am 21. November
+elif month == 7 and day == 19:                 # genau am 19. Juli
     print("HERZLICHEN GLÜCKWUNSCH!")
     print("Sie haben heute Geburtstag!")
-else:                                           # nach dem 21. November bleibt übrig
+else:                                           # nach dem 19. Juli bleibt übrig
     print("Sie hatten dieses Jahr noch nicht Geburtstag.")
 
-if month < 7 or (month == 7 and day <= 18):   # vor November oder vor oder am 21. November
+if month < 7 or (month == 7 and day <= 19):   # vor Juli oder vor oder am 19. Juli
     print("Sie sind", 2017 - year, "Jahre alt.")
-else:                                           # nach dem 21. November
+else:                                           # nach dem 19. Juli
     print("Sie sind", 2016 - year, "Jahre alt.")
